433e7a24d94192f3f1f73e8331a0476786d28db9
patriciapossarii <patricia.mag.carvalho@gmail.com>
2022-09-08 20:31:52 -0300

    Adicionando os 3 exercícios resolvidos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,44 @@
-const num = //seu numero aqui
 
+// # Exercício 1
+
+// Crie um código que receba um número por **prompt** e 
+// verifique se um número é divisível por 2 **ou** por 3
+
+// Faça isso:
+
+const numero = Number(prompt("Digite um número:"))
+console.log("Valor digitado: ", numero)
+
+//1. Utilizando ifs aninhados
+
+if (numero % 2 === 0) {
+    if (numero % 3 === 0) {
+        console.log("O número é divisivel por 2 ou por 3")
+    }
+    else {
+        console.log("O número é divisivel por 2 ou por 3")
+    }
+}
+else {
+    if (numero % 3 === 0) {
+        console.log("O número é divisivel por 2 ou por 3")
+    }
+    else {
+
+        console.log("O número informado NÃO é divisivel por 2 ou por 3")
+    }
+}
+
+
+/*
+
+//2. Utilizando um operador lógico para unir duas operações relacionais:
+
+if (numero % 2 == 0 || numero % 3 == 0) {
+    console.log("O número é divisivel por 2 ou por 3")
+}
+else {
+    console.log("O número informado NÃO é divisivel por 2 ou por 3")
+}
+
+*/
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,5 +1,22 @@
-//código a ser reescrito
+/*# Exercício 3
 
+Reescreva os códigos do arquivo script.js utilizando o if ternário.
+
+**Relembrando o if ternário**
+```jsx
+condição ? expr1 : expr2
+```
+
+Onde :
+
+`condição` é uma expressão que é avaliada como `true` ou `false`.
+
+`expr1`, `expr2` são xpressões com valores de qualquer tipo.
+*/
+
+
+//código a ser reescrito
+/*
 if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
@@ -11,3 +28,16 @@ if(idade >= 18){
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
 }
+*/
+
+const nome = prompt("Informe o nome:").toUpperCase();
+const idade = prompt("Informe a idade: ");
+
+console.log(`
+Nome e idade informada: ${nome}, ${idade} anos`)
+
+nome === 'JOSÉ'? console.log("Oi, Zé!"): console.log("Olá",nome)
+idade >= 18 ? console.log("Pode tirar carteira de motorista!") : console.log("Não pode tirar carteira de motorista!");
+
+
+
